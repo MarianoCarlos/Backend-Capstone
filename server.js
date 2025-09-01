d534fd432b56c04260cf44f13a28b95cb9f56946
carlos <carlosjirehmariano@gmail.com>
2025-09-01 23:43:04 +0800

    server

--- server.js
+++ server.js
@@ -22,21 +22,40 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
 	console.log("New user connected:", socket.id);
 
+	// Join room
 	socket.on("join-room", (room) => {
 		socket.join(room);
+		console.log(`${socket.id} joined room ${room}`);
+		// Notify all others in the room
 		socket.to(room).emit("new-user", socket.id);
 	});
 
+	// Forward offer
 	socket.on("offer", ({ sdp, to }) => {
 		io.to(to).emit("offer", { sdp, from: socket.id });
 	});
 
+	// Forward answer
 	socket.on("answer", ({ sdp, to }) => {
 		io.to(to).emit("answer", { sdp, from: socket.id });
 	});
 
+	// Forward ICE candidate
 	socket.on("ice-candidate", ({ candidate, to }) => {
 		io.to(to).emit("ice-candidate", { candidate });
+	});
+
+	// End call
+	socket.on("end-call", ({ to }) => {
+		io.to(to).emit("end-call"); // Notify remote
+	});
+
+	// Disconnect
+	socket.on("disconnecting", () => {
+		const rooms = [...socket.rooms].filter((r) => r !== socket.id);
+		rooms.forEach((room) => {
+			socket.to(room).emit("end-call"); // Notify all peers in the room
+		});
 	});
 
 	socket.on("disconnect", () => {
